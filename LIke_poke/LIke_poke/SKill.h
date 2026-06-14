557c9930ab38e7cd0dc062e4e9cb671365c80cd7
vemapek <74702232+vemapek@users.noreply.github.com>
2026-06-14 22:54:19 +0900

    Update SKill.h

--- LIke_poke/LIke_poke/SKill.h
+++ LIke_poke/LIke_poke/SKill.h
@@ -3,10 +3,11 @@
 #include <string>
 using namespace std;
 
+// 스킬 구조체 - 크리처가 사용하는 기술 데이터
 struct Skill
 {
-	string Name;
-	Type SkillType;
-	int Damage;
-	bool Ult;
+    string Name;        // 스킬 이름
+    Type SkillType;     // 스킬 타입 (상성 계산에 사용)
+    int Damage;         // 기본 데미지 수치
+    bool Ult;           // 필살기 여부 (true면 필살기)
 };
