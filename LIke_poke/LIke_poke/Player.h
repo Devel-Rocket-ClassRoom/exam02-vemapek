6ec5ab1d523974e18482730a329b172b1f1a9c80
vemapek <74702232+vemapek@users.noreply.github.com>
2026-06-14 22:55:26 +0900

    Update Player.h

--- LIke_poke/LIke_poke/Player.h
+++ LIke_poke/LIke_poke/Player.h
@@ -2,18 +2,18 @@
 #include "Creature.h"
 #include "Item.h"
 
+// 플레이어 클래스 - 플레이어의 모든 상태 관리
 class Player
 {
 public:
-	string Name;
-	Creature* Owned[10];
-	Creature* Active;
-	int OwnedSize;
-	Bag PlayerBag;
+    string Name;            // 플레이어 이름
+    Creature* Owned[10];    // 보유 크리처 배열 (최대 10마리)
+    Creature* Active;       // 현재 필드에 출전 중인 크리처 포인터
+    int OwnedSize;          // 현재 보유 크리처 수
 
-	bool Pokedex[10] = { false };
-	int PokedexCount = 0;
-	bool GameClear = false;
+    Bag PlayerBag;          // 플레이어 가방
 
+    bool Pokedex[10] = { false };  // 도감 배열 (인덱스별 포획 여부)
+    int PokedexCount = 0;          // 도감 등록 수
+    bool GameClear = false;        // 10마리 수집 시 true로 변경
 };
-
