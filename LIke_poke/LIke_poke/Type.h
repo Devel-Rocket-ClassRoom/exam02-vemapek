cf44386dc5e67ff0ffe1a7b6fc911e99d0c62209
vemapek <74702232+vemapek@users.noreply.github.com>
2026-06-15 10:13:41 +0900

    Add files via upload

---
+++ LIke_poke/LIke_poke/Type.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+//크리처와 스킬의 속성 정의
+enum class Type { Fire, Water, Rock, Grass, Electric };
+
+// 공격 타입과 방어 타입을 받아 데미지 배율 반환
+// 반환값: 2.0 (효과적) / 1.0 (보통) / 0.5 (비효과적)
+float GetTypeMultiplier(Type attackType, Type defenseType);
